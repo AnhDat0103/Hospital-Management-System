e55963bff7b419afef49f2a52399e53c2430236b
Nguyễn Trọng Đạt <124783881+AnhDat0103@users.noreply.github.com>
2024-06-12 10:52:02 +0700

    test checking push to developing branch.

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Hospital Management System");
+        System.out.println("Take care the everyone's health together.");
     }
 }
