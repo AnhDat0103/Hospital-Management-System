090c58c4b0d4e0d2f98b718c3d8aee3664391ac0
kimtuanhg <tuankdhe186039@fpt.edu.vn>
2024-06-12 11:34:38 +0700

    create attributes in Doctor class

--- src/models/Doctor.java
+++ src/models/Doctor.java
@@ -1,4 +1,26 @@
 package models;
 
+import java.util.HashMap;
+
 public class Doctor {
+    private int yearsOfExperience;
+    private int clinicHours;
+    private String education;
+    private Specialization specialization;
+    private double consultationFee;
+    private HashMap patients;
+
+    public Doctor(){
+    }
+    public Doctor(int yearsOfExperience, int clinicHours,String education, Specialization specialization, double consultationFee, HashMap patients){
+        this.yearsOfExperience = yearsOfExperience;
+        this.clinicHours = clinicHours;
+        this.education = education;
+        this.specialization = specialization;
+        this.consultationFee = consultationFee;
+        this.patients = new HashMap();
+    }
+
+
+
 }
