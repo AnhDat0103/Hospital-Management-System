db203ca54bf0828c3cf2911728f396beefc5cbcc
tuananh1712 <Nguyentuananh171220041@gmail.com>
2024-06-12 11:41:36 +0700

    test push feature version 1

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Hospital Management System");
         System.out.println("Take care the everyone's health together.");
+        System.out.println("hihihh");
     }
 }
